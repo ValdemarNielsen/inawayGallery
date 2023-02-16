afdb62da929b61c5adc076c4f8858c9732895dd8
Valdemar Nielsen <v.nielsen@live.dk>
2023-02-16 13:23:01 +0100

    previous picture function no longer works. next picture now iterates through an array where new pictures can easily be added. Buttons got updated visuals

--- main.js
+++ main.js
@@ -5,6 +5,42 @@ document.getElementById("pictureName")
 
 let myHeading = document.querySelector("h1");
 
+let arrayOfImagee = [];
+arrayOfImagee.push('src/assets/pictures/abstractredone.jpg');
+arrayOfImagee.push('src/assets/pictures/africanwoman.jpg', 'title 2');
+arrayOfImagee.push('src/assets/pictures/bloodcircles.jpg', 'title 3');
+arrayOfImagee.push('src/assets/pictures/coupleholdinghands.jpg', 'title 4');
+arrayOfImagee.push('src/assets/pictures/curvesinsand.jpg', 'title 5');
+arrayOfImagee.push('src/assets/pictures/geometry.jpg', 'title 6');
+arrayOfImagee.push('src/assets/pictures/infinity.jpg', 'title 7');
+arrayOfImagee.push('src/assets/pictures/jesus101.jpg', 'title y');
+arrayOfImagee.push('src/assets/pictures/peopledancing.jpg', 'title 8');
+arrayOfImagee.push('src/assets/pictures/redpainting.jpg', 'title 9');
+arrayOfImagee.push('src/assets/pictures/redaquarefaces.jpg', 'title 10');
+arrayOfImagee.push('src/assets/pictures/twofriends.jpg', 'title 11');
+arrayOfImagee.push('src/assets/pictures/valdesbillede.jpg', 'title 12');
+arrayOfImagee.push('src/assets/pictures/valentine.jpg', 'titel 13');
+
+var arrayOfImages = ["src/assets/pictures/abstractredone.jpg", "src/assets/pictures/africanwoman.jpg", "src/assets/pictures/bloodcircles.jpg", "src/assets/pictures/coupleholdinghands.jpg",
+"src/assets/pictures/curvesinsand.jpg", "src/assets/pictures/geometry.jpg", "src/assets/pictures/infinity.jpg", "src/assets/pictures/peopledancing.jpg",
+"src/assets/pictures/redpainting.jpg", "src/assets/pictures/redsquarefaces.jpg", "src/assets/pictures/twofriends.jpg", "src/assets/pictures/valdesbillede.jpg",
+"src/assets/pictures/valentine.jpg"]
+var currentImage = 0;
+function pictureChangerNext() {
+    currentImage ++;
+    if (currentImage >= arrayOfImages.length) {
+        currentImage = 0;
+    }
+    myImage.setAttribute("src", arrayOfImages[currentImage])
+}
+function pictureChangerPrev() {
+    currentImage --;
+    if (currentImage === 1) {
+        currentImage = arrayOfImages.length;
+    }
+    myImage.setAttribute("src", arrayOfImages[currentImage])
+}
+
 if (!localStorage.getItem("name")) {
     setUserName();
 } else {
